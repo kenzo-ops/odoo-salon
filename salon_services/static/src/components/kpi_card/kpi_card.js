bba5d730d85bf32f3a6e52f9d1e6d237be6f5a23
daffff2927 <faiqwidayat@gmail.com>
2025-08-13 12:50:49 +0700

    clickable component dan penambahan tabel

--- salon_services/static/src/components/kpi_card/kpi_card.js
+++ salon_services/static/src/components/kpi_card/kpi_card.js
@@ -4,18 +4,33 @@ import { Component, useState, onWillStart } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 
 export class ServiceKpiCard extends Component {
-    static props = ["img", "title", "model"];
+  static props = ["img", "title", "model", "clickable"];
 
-    setup() {
-        this.orm = useService("orm");
-        this.state = useState({ total: 0 });
+  setup() {
+    this.orm = useService("orm");
+    this.action = useService("action");
+    this.state = useState({ total: 0 });
 
-        onWillStart(async () => {
-            const total = await this.orm.searchCount(this.props.model, []);
-            this.state.total = total;
-        });
+    onWillStart(async () => {
+      const total = await this.orm.searchCount(this.props.model, []);
+      this.state.total = total;
+    });
+  }
+
+  openListView() {
+    if (this.props.clickable) {
+      this.action.doAction({
+        type: "ir.actions.act_window",
+        name: this.props.title,
+        res_model: this.props.model,
+        views: [
+          [false, "list"],
+          [false, "form"],
+        ],
+        target: "current",
+      });
     }
+  }
 }
 
 ServiceKpiCard.template = "owl.ServiceKpiCard";
-
--- salon_services/static/src/components/list_table/list_table.js
+++ salon_services/static/src/components/list_table/list_table.js
@@ -4,8 +4,68 @@ import { Component, useState, onWillStart } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 
 export class ListBookingTable extends Component {
-    setup() {   
-    }
+  setup() {
+    this.orm = useService("orm");
+    this.actionService = useService("action");
+    this.state = useState({ bookings: [] });
+
+    // Bind agar this di openBookingDetail tetap benar
+    this.openBookingDetail = this.openBookingDetail.bind(this);
+
+    onWillStart(async () => {
+      const records = await this.orm.searchRead(
+        "salon.booking",
+        [],
+        [
+          "booking_id",
+          "customer",
+          "service_booking_id",
+          "booking_date",
+          "state",
+          "total_price",
+        ]
+      );
+
+      const allServiceBookingIds = records.flatMap(
+        (rec) => rec.service_booking_id || []
+      );
+
+      let servicesById = {};
+      if (allServiceBookingIds.length) {
+        const serviceBookings = await this.orm.read(
+          "salon.booking.service",
+          allServiceBookingIds,
+          ["service_id"]
+        );
+        servicesById = Object.fromEntries(
+          serviceBookings.map((s) => [s.id, s.service_id?.[1] || ""])
+        );
+      }
+
+      this.state.bookings = records.map((rec, idx) => ({
+        id: rec.id,
+        no: idx + 1,
+        booking_id: rec.booking_id,
+        customer: rec.customer?.[1] || "",
+        service:
+          (rec.service_booking_id && servicesById[rec.service_booking_id[0]]) ||
+          "",
+        booking_date: rec.booking_date ? rec.booking_date.split(" ")[0] : "",
+        state: rec.state,
+        total_price: rec.total_price,
+      }));
+    });
+  }
+
+  openBookingDetail(bookingId) {
+    this.actionService.doAction({
+      type: "ir.actions.act_window",
+      res_model: "salon.booking",
+      res_id: bookingId,
+      views: [[false, "form"]],
+      target: "current",
+    });
+  }
 }
 
 ListBookingTable.template = "owl.ListBookingTable";
